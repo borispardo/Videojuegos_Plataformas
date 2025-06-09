746ee563d0889e17d4752dda0c2761a7cae9f1fa
sirob <boris.pardo8021@utc.edu.ec>
2025-06-09 09:48:05 -0500

    6. Comit: Creacion y configuracion de Plataforma.aspx.cs

--- Videojuegos_Plataformas/Plataformas.aspx.cs
+++ Videojuegos_Plataformas/Plataformas.aspx.cs
@@ -1,17 +1,103 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
 
 namespace Videojuegos_Plataformas
 {
     public partial class Plataformas : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
 
+        protected void Page_Load(object sender, EventArgs e)  
+        {
+            if (!IsPostBack)
+            {
+                CargarPlataformas();
+            }
+        }
+
+        private void CargarPlataformas()
+        {
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Plataforma", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvPlataformas.DataSource = dt;
+                gvPlataformas.DataBind();
+            }
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                con.Open();
+                string query;
+
+                if (string.IsNullOrEmpty(hfPlataformaID.Value))
+                {
+                    query = "INSERT INTO Plataforma (Nombre) VALUES (@Nombre)";
+                }
+                else
+                {
+                    query = "UPDATE Plataforma SET Nombre=@Nombre WHERE PlataformaID=@ID";
+                }
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                    if (!string.IsNullOrEmpty(hfPlataformaID.Value))
+                        cmd.Parameters.AddWithValue("@ID", hfPlataformaID.Value);
+
+                    cmd.ExecuteNonQuery();
+                }
+                LimpiarFormulario();
+                CargarPlataformas();
+            }
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtNombre.Text = "";
+            hfPlataformaID.Value = "";
+        }
+
+        protected void gvPlataformas_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewRow fila = gvPlataformas.Rows[e.NewEditIndex];
+            hfPlataformaID.Value = gvPlataformas.DataKeys[e.NewEditIndex].Value.ToString();
+            txtNombre.Text = fila.Cells[0].Text;
+        }
+
+        protected void gvPlataformas_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(gvPlataformas.DataKeys[e.RowIndex].Value);
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Plataforma WHERE PlataformaID = @ID", con);
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.ExecuteNonQuery();
+            }
+            CargarPlataformas();
+        }
+
+        protected void gvPlataformas_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            LimpiarFormulario();
+        }
+
+        protected void gvPlataformas_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            // Vacío intencionalmente: el botón de edición carga el formulario superior
         }
     }
 }
