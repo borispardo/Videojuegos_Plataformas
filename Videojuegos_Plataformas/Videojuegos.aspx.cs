6c2e605daee73183474aaa6393f56911332fee9b
sirob <boris.pardo8021@utc.edu.ec>
2025-06-09 09:36:12 -0500

    3. Comit: Creacion y configuracion de la tabla Videojuegos.aspx

---
+++ Videojuegos_Plataformas/Videojuegos.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace Videojuegos_Plataformas
+{
+    public partial class Videojuegos : System.Web.UI.Page
+    {
+        string conexion = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarVideojuegos();
+            }
+        }
+
+        private void CargarVideojuegos()
+        {
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Videojuego", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvVideojuegos.DataSource = dt;
+                gvVideojuegos.DataBind();
+            }
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                con.Open();
+                string query;
+
+                if (string.IsNullOrEmpty(hfID.Value))
+                {
+                    // Insertar
+                    query = "INSERT INTO Videojuego (Titulo, Desarrollador, Lanzamiento) VALUES (@Titulo, @Desarrollador, @Lanzamiento)";
+                }
+                else
+                {
+                    // Actualizar
+                    query = "UPDATE Videojuego SET Titulo=@Titulo, Desarrollador=@Desarrollador, Lanzamiento=@Lanzamiento WHERE VideojuegoID=@ID";
+                }
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Titulo", txtTitulo.Text);
+                    cmd.Parameters.AddWithValue("@Desarrollador", txtDesarrollador.Text);
+                    cmd.Parameters.AddWithValue("@Lanzamiento", txtFecha.Text);
+
+                    if (!string.IsNullOrEmpty(hfID.Value))
+                        cmd.Parameters.AddWithValue("@ID", hfID.Value);
+
+                    cmd.ExecuteNonQuery();
+                }
+                LimpiarFormulario();
+                CargarVideojuegos();
+            }
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtTitulo.Text = "";
+            txtDesarrollador.Text = "";
+            txtFecha.Text = "";
+            hfID.Value = "";
+        }
+
+        protected void gvVideojuegos_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewRow fila = gvVideojuegos.Rows[e.NewEditIndex];
+            hfID.Value = gvVideojuegos.DataKeys[e.NewEditIndex].Value.ToString();
+            txtTitulo.Text = fila.Cells[0].Text;
+            txtDesarrollador.Text = fila.Cells[1].Text;
+            txtFecha.Text = DateTime.Parse(fila.Cells[2].Text).ToString("yyyy-MM-dd");
+        }
+
+        protected void gvVideojuegos_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(gvVideojuegos.DataKeys[e.RowIndex].Value);
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Videojuego WHERE VideojuegoID = @ID", con);
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.ExecuteNonQuery();
+            }
+            CargarVideojuegos();
+        }
+
+        protected void gvVideojuegos_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            LimpiarFormulario();
+        }
+
+        protected void gvVideojuegos_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            // Esta función no se usa porque el botón "Editar" carga datos en el formulario para luego guardar.
+            // Pero puedes dejarla vacía o redirigirla a btnGuardar_Click si quieres usar edición en línea.
+        }
+    }
+}
---
+++ Videojuegos_Plataformas/Videojuegos.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Videojuegos_Plataformas
+{
+
+
+    public partial class Videojuegos
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control hfID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hfID;
+
+        /// <summary>
+        /// Control lblTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitulo;
+
+        /// <summary>
+        /// Control txtTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTitulo;
+
+        /// <summary>
+        /// Control lblDesarrollador.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDesarrollador;
+
+        /// <summary>
+        /// Control txtDesarrollador.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDesarrollador;
+
+        /// <summary>
+        /// Control lblFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFecha;
+
+        /// <summary>
+        /// Control txtFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFecha;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+
+        /// <summary>
+        /// Control btnCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancelar;
+
+        /// <summary>
+        /// Control gvVideojuegos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvVideojuegos;
+    }
+}
